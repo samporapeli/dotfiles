294456b06923c869dc3c06a2ef27171754edaffe
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-09-28 15:22:55 +0300

    Add better installation script

--- Documents/setup_dotfiles.sh
+++
@@ -1,8 +0,0 @@
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME/"
-echo ".dotfiles" >> .gitignore
-git clone --bare git@github.com:samporapeli/dotfiles.git $HOME/.dotfiles
-mkdir -p .dotfiles-backup && dotfiles checkout 2>&1 |
-    egrep "\s+\." |
-    awk {"print $1"} |
-    xargs -I{} mv {} .dotfiles-backup/{}
-dotfiles config --local status.ShowUntrackedFiles no
