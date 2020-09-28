207db115b13bad611ef8975e52f286d9676cdaf6
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-09-28 17:20:19 +0300

    Add more setting up to installation

--- Documents/install-dotfiles.sh
+++ Documents/install-dotfiles.sh
@@ -28,13 +28,30 @@ function install_dotfiles() {
         echo -n "Destroy previous configuration? This cannot be undone. [y/N] "
         read DESTROY_CONFIRMATION
         if [ "$DESTROY_CONFIRMATION" = "y" ]; then
+            echo Copying files...
             dotfiles checkout
+            echo Downloading git submodules...
+            dotfiles submodule init
+            dotfiles submodule update
         else
             echo Aborting...
             exit
         fi
         elif [ "$INSTALL_TYPE" = "clone" ]; then
             git clone $GIT_PREFIX$GIT_PATH
+        fi
+        echo -n "Install zsh? [Y/n] "
+        read INSTALL_ZSH
+        if [ "INSTALL_ZSH" = "" ] || [ "$INSTALL_ZSH" = "y" ] && command -v apt-get &> /dev/null; then
+            echo Using apt-get to install zsh...
+            sudo apt-get -y install zsh
+        else
+            echo apt-get not found. Please install zsh manually.
+        fi
+        echo -n "Install oh-my-zsh? (https://ohmyz.sh) [Y/n] "
+        read OH_MY_ZSH
+        if [ "$OH_MY_ZSH" = "" ] || [ "$OH_MY_ZSH" = "y" ]; then
+            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         fi
     elif [ "$INSTALL_TYPE" = "exit" ]; then
         exit
