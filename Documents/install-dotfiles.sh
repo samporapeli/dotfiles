f688210e89eeb7b68715185be914faef54996276
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-09-28 23:12:42 +0300

    Add zsh and oh-my-zsh installation and fix indent

--- Documents/install-dotfiles.sh
+++ Documents/install-dotfiles.sh
@@ -18,6 +18,21 @@ function install_dotfiles() {
             echo Invalid input, exiting
             exit
         fi
+        if command -v apt-get &> /dev/null && ! command -v zsh &> /dev/null; then
+            echo -n "Install zsh? [Y/n] "
+            read INSTALL_ZSH
+            if [ "$INSTALL_ZSH" = "" ] || [ "$INSTALL_ZSH" = "y" ]; then
+                echo Using apt-get to install zsh...
+                sudo apt-get -y install zsh
+            fi
+        fi
+        echo Installing of oh-my-zsh (https://ohmyz.sh)
+        echo After oh-my-zsh installation you must exit the zsh shell, e. g. by typing exit and pressing enter.
+        echo -n "Install? (remember to exit) [Y/n] "
+        read OH_MY_ZSH
+        if [ "$OH_MY_ZSH" = "" ] || [ "$OH_MY_ZSH" = "y" ]; then
+            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        fi
         if [ "$INSTALL_TYPE" = "replace" ]; then
             function dotfiles () {
                 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME/ $@
@@ -25,33 +40,20 @@ function install_dotfiles() {
             echo ".dotfiles" >> .gitignore
             dotfiles clone --bare $GIT_PREFIX$GIT_PATH $HOME/.dotfiles
             dotfiles config --local status.showUntrackedFiles no
-        echo -n "Destroy previous configuration? This cannot be undone. [y/N] "
-        read DESTROY_CONFIRMATION
-        if [ "$DESTROY_CONFIRMATION" = "y" ]; then
-            echo Copying files...
-            dotfiles checkout
-            echo Downloading git submodules...
-            dotfiles submodule init
-            dotfiles submodule update
-        else
-            echo Aborting...
-            exit
-        fi
+            echo -n "Destroy previous configuration? This cannot be undone. [y/N] "
+            read DESTROY_CONFIRMATION
+            if [ "$DESTROY_CONFIRMATION" = "y" ]; then
+                echo Copying files...
+                dotfiles checkout -f master
+                echo Downloading git submodules...
+                dotfiles submodule init
+                dotfiles submodule update
+            else
+                echo Aborting...
+                exit
+            fi
         elif [ "$INSTALL_TYPE" = "clone" ]; then
             git clone $GIT_PREFIX$GIT_PATH
-        fi
-        echo -n "Install zsh? [Y/n] "
-        read INSTALL_ZSH
-        if [ "INSTALL_ZSH" = "" ] || [ "$INSTALL_ZSH" = "y" ] && command -v apt-get &> /dev/null; then
-            echo Using apt-get to install zsh...
-            sudo apt-get -y install zsh
-        else
-            echo apt-get not found. Please install zsh manually.
-        fi
-        echo -n "Install oh-my-zsh? (https://ohmyz.sh) [Y/n] "
-        read OH_MY_ZSH
-        if [ "$OH_MY_ZSH" = "" ] || [ "$OH_MY_ZSH" = "y" ]; then
-            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         fi
     elif [ "$INSTALL_TYPE" = "exit" ]; then
         exit
