65e1bca964c3a1e542dfffd8d733d7cbe7625d62
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2023-04-14 10:59:43 +0300

    Add useful LSP configuration

--- .config/nvim/lua/ide/init.lua
+++ .config/nvim/lua/ide/init.lua
@@ -1,4 +1,52 @@
 require("ide.plugins")
 
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+  }
+})
+
+local cmp = require("cmp")
+cmp.setup({
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  }, {
+    {
+      { name = "buffer" },
+    },
+  }),
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+})
+
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lsp_attach = function(client, bufnr)
+  -- custom keybindings here
+end
+
+local lspconfig = require("lspconfig")
+for _, server_name in ipairs(require("mason-lspconfig").get_installed_servers()) do
+  lspconfig[server_name].setup({
+    on_attach = lsp_attach,
+    capabilities = lsp_capabilities,
+  })
+end
+
+-- arduino
+-- if file doesn't exist, run `arduino-cli config init`
+-- vim.env.ARDUINOCLI_CONFIG_PATH = os.getenv("HOME") .. "/.arduino15/arduino-cli.yaml"
--- .config/nvim/lua/ide/plugins.lua
+++ .config/nvim/lua/ide/plugins.lua
@@ -5,4 +5,9 @@ return packer.startup(function (use)
   use "neovim/nvim-lspconfig"
   use "williamboman/mason.nvim"
   use "williamboman/mason-lspconfig.nvim"
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-vsnip"
+  use "hrsh7th/vim-vsnip"
 end)
