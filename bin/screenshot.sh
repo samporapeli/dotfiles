f37d81ba46b2e3cd270c9e483056e6149990da4e
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2024-08-21 16:15:22 +0300

    Add support for Wayland

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -eo pipefail
+
 SS_DIRECTORY="$HOME/Pictures/screenshot.sh"
 FILENAME="$(date --iso-8601=ns)_screenshot.png"
 FILE="$SS_DIRECTORY/$FILENAME"
 mkdir -p "$SS_DIRECTORY"
 
+IS_WAYLAND=$([[ "$XDG_SESSION_TYPE" = "wayland" ]])
+
 if [[ "$1" == "select" ]]; then
-    scrot -s -F "$FILE"
+    $IS_WAYLAND && gnome-screenshot --area --file="$FILE" || scrot -s -F "$FILE"
 elif [[ "$1" == "full" ]] || [[ "$1" == "" ]]; then
-    scrot -F "$FILE"
+    $IS_WAYLAND && gnome-screenshot --file="$FILE" || scrot -F "$FILE"
 else
     echo "Unknown parameter '$1'"
     echo ""
@@ -18,14 +22,19 @@ else
     exit 1
 fi
 
-xclip -selection clipboard -t image/png -i "$FILE" &&
-    NOTIFICATION_RESULT=$(
-        notify-send \
-            -a 'screenshot.sh' \
-            "Image \"$FILE\" copied to clipboard" \
-            --action=KEEP="Keep and close" \
-            --action=DELETE="Delete the file" \
-    )
+if $IS_WAYLAND; then
+    wl-copy < "$FILE"
+else
+    xclip -selection clipboard -t image/png -i "$FILE"
+fi
+
+NOTIFICATION_RESULT=$(
+    notify-send \
+        -a 'screenshot.sh' \
+        "Image \"$FILE\" copied to clipboard" \
+        --action=KEEP="Keep and close" \
+        --action=DELETE="Delete the file" \
+)
 
 if [[ "$NOTIFICATION_RESULT" = "DELETE" ]]; then
     rm "$FILE"
