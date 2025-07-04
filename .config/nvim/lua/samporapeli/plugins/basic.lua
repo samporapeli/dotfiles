4aedcb020d4c15651ae4290375691f2ad1bdd844
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2025-07-04 15:42:13 +0300

    Migrate from VimAirline to lualine

--- .config/nvim/lua/samporapeli/plugins/basic.lua
+++ .config/nvim/lua/samporapeli/plugins/basic.lua
@@ -5,12 +5,10 @@ return {
 	"tpope/vim-repeat",
 	"tpope/vim-surround",
 	{
-		"vim-airline/vim-airline",
-		init = function()
-			vim.g.airline_theme = "catppuccin"
-			vim.g.airline_powerline_fonts = 1
-			vim.g["airline#extensions#tabline#enabled"] = 1
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('lualine').setup()
 		end
 	},
-	"vim-airline/vim-airline-themes",
 }
