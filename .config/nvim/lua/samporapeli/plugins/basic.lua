2c96d3a769166252dc804f5402e606d5a0e9d59f
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2025-07-30 17:26:08 +0300

    Enable tabline with buffers

--- .config/nvim/lua/samporapeli/plugins/basic.lua
+++ .config/nvim/lua/samporapeli/plugins/basic.lua
@@ -8,7 +8,16 @@ return {
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
-			require('lualine').setup()
+			require('lualine').setup({
+				tabline = {
+					lualine_a = {'buffers'},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {'tabs'}
+				}
+			})
 		end
 	},
 }
