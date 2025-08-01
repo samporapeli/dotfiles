32be867211ef21f5ce04c94d1cd4a7a1d87d164b
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2025-08-01 12:12:43 +0300

    Fix LSP setup (Lua)

--- .config/nvim/lua/samporapeli/ide/init.lua
+++ .config/nvim/lua/samporapeli/ide/init.lua
@@ -17,7 +17,11 @@ require("mason").setup({
 		icons = mason_icons
 	},
 })
-require("mason-lspconfig").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls", "pyright", "eslint", "ts_ls" },
+	automatic_installation = true,
+	automatic_enable = true,
+})
 
 -- Github Copilot
 require("samporapeli.ide.copilot")
@@ -62,13 +66,44 @@ vim.keymap.set("n", "<Leader>dl", vim.diagnostic.setqflist)
 vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<Leader><F2>", vim.lsp.buf.rename)
 
+-- Setup lua_ls with vim.lsp.config
+vim.lsp.config('lua_ls', {
+	on_attach = lsp_attach,
+	capabilities = lsp_capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+
+-- Setup other servers with lspconfig
 local lspconfig = require("lspconfig")
 for _, server_name in ipairs(require("mason-lspconfig").get_installed_servers()) do
-	lspconfig[server_name].setup({
-		on_attach = lsp_attach,
-		capabilities = lsp_capabilities,
-	})
+	if server_name ~= "lua_ls" then -- Skip lua_ls, handled above
+		lspconfig[server_name].setup({
+			on_attach = lsp_attach,
+			capabilities = lsp_capabilities,
+		})
+	end
 end
+
+-- Start LSP for existing buffers that were opened before config loaded
+vim.schedule(function()
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].filetype ~= "" then
+			vim.api.nvim_buf_call(bufnr, function()
+				-- Trigger FileType event to start appropriate LSP
+				vim.cmd("doautocmd FileType " .. vim.bo.filetype)
+			end)
+		end
+	end
+end)
 
 if vim.bo.filetype == "dart" then
 	require("samporapeli.ide.dart")
